5de394130b6f6e4c4cc20b1454483f390544cc55
risingone <anuragdhingra101@gmail.com>
2018-03-19 04:10:54 +0530

    post api to create meetups

--- meetup-backend/server/index.js
+++ meetup-backend/server/index.js
@@ -1,10 +1,19 @@
 import express from 'express';
 import dbConfig from './config/db';
+import middlewaresConfig from './config/middlewares';
+import { MeetupRoutes } from './modules';
 
 const app = express();
 
 // Database
 dbConfig();
+
+//Middlewares
+middlewaresConfig(app);
+
+app.use('/api',[MeetupRoutes]);
+
+
 
 const PORT = process.env.PORT || 3000;
 
