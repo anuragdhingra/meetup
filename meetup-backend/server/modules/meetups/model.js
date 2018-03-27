0eb5af1b491cfa8aa96ea9358252338e788995b0
risingone <anuragdhingra101@gmail.com>
2018-03-27 21:06:19 +0530

    groups can now create meetups

--- meetup-backend/server/modules/meetups/model.js
+++ meetup-backend/server/modules/meetups/model.js
@@ -3,12 +3,21 @@ import mongoose, { Schema } from 'mongoose';
 const MeetupSchema = new Schema({
     title: {
         type: String,
-        required: true
+        required: true,
+        minLength: [5, 'Title must be 5 characters long']
     },
     description: {
         type: String,
-        required: true
+        required: true,
+        minLength: [10, 'Description must be 5 characters long']
+    },
+    eventDate: {
+        type: Date
+    },
+    group: {
+        type: Schema.Types.ObjectId,
+        ref:'Group'
     }
-});
+}, {timestamps: true}); // to sort meetups acc to date
 
 export default mongoose.model('Meetup',MeetupSchema);
